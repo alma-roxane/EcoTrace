0efc99bd56f754bca1dc8dc8ab708f5b75dae0f1
alma-roxane <almaroxane27@gmail.com>
2026-03-19 09:22:15 +0530

    Updated the main function pdf_parser.py

--- modules/pdf_parser.py
+++ modules/pdf_parser.py
@@ -2,3 +2,24 @@ import pdfplumber
 import pandas as pd
 import re
 import os
+
+def load_grid_emission(csv_path):
+    df = pd.read_csv(csv_path)
+    return df
+
+
+if __name__ == "__main__":
+    
+
+    #Path 
+    PDF_PATH = "/home/user/Desktop/Projects/PBL/data/sample_reports/MCFAR2024-25FINAL.pdf"
+    CSV_PATH = "/home/user/Desktop/Projects/PBL/data/grid_emission_factors.csv"
+
+    #Run 
+    result = parse_pdf(PDF_PATH, CSV_PATH)
+    
+    if result:
+        print("Parsing successful!")
+
+    else:
+        print("Parsing failed - check file paths.")
